05554a745d73e51b0d922bf9989a9048bac296df
David Herring <davidherring@Davids-MBP.home>
2023-04-09 01:10:26 -0500

    fixing bug

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
-import Footer from './components/Footer'
-import Login from './pages/login'
-import Register from './pages/register';
+import Footer from "./components/Footer";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
     <>
       <Header />
-      <Register />
       <Router>
-        <Routes>
-        </Routes>
+        <Routes path="/" element={<Login />}></Routes>
+        <Routes path="/register" element={<Register />}></Routes>
       </Router>
       <Footer />
     </>
